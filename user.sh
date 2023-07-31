00f094e5a8b4a3ae90616815c5c10147caa6064e
github-repos <sheshankprakash260@gmail.com>
2023-07-31 20:32:26 +0530

    Frontend code

---
+++ user.sh
@@ -0,0 +1,27 @@
+
+
+cp user.service /etc/systemd/system/user.service
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+
+
+cd /app
+npm install
+
+systemctl enable user
+systemctl start user
+
+yum install mongodb-org-shell -y
+
+mongo --host mongodb.pdevopst74.online </app/schema/user.js
