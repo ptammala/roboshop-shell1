7069eb11bb35a01cf4ed0f85d4dd1d1d3bde1e4a
github-repos <sheshankprakash260@gmail.com>
2023-08-01 19:47:29 +0530

    Updated manual things using sed

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+
 systemctl enable mongod
 systemctl restart mongod
--- redis.sh
+++ redis.sh
@@ -6,6 +6,8 @@ yum module enable redis:remi-6.2 -y
 
 yum install redis -y
 
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf /etc/redis.conf
+
 systemctl enable redis
 systemctl restart redis
 
