bf312e0d3dcf6778e8cb87e5aa8a17231c708639
github-repos <sheshankprakash260@gmail.com>
2023-08-01 23:19:17 +0530

    Functions using for catalogue and user

--- common.sh
+++ common.sh
@@ -42,51 +42,51 @@ func_apppreq(){
 
   echo -e "\e[31m>>>>>>  Create Application User <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
 
-  useradd roboshop
+  useradd roboshop &>> ${log}
 
   echo -e "\e[31m>>>>>>  Cleaning Application  Content <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
   rm -rf /app &>> ${log}
 
   echo -e "\e[31m>>>>>>  Create Application Directory <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
 
-  mkdir /app
+  mkdir /app &>> ${log}
 
   echo -e "\e[31m>>>>>>  Downloading  Application Content <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
 
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log}
+  cd /app &>> ${log}
 
   echo -e "\e[31m>>>>>>  Extracting  Application Content  <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
-  unzip /tmp/${component}.zip
+  unzip /tmp/${component}.zip &>> ${log}
 
 }
 
 func_java(){
 
-log=/tmp/roboshop.log
+log=/tmp/roboshop.log &>> ${log}
 
 echo -e "\e[31m>>>>>>  Installing Maven <<<<<<<<<<<<<\e[0m" | tee -a ${log}
 
-yum install maven -y
+yum install maven -y &>> ${log}
 
 echo -e "\e[31m>>>>>>  Create  a  ${component} Service File <<<<<<<<<<<<<\e[0m" | tee -a ${log}
 
-cp shipping.service /etc/systemd/system/${component}.service
+cp shipping.service /etc/systemd/system/${component}.service &>> ${log}
 
 func_apppreq
 
 echo -e "\e[31m>>>>>>  Building  a  ${component} Service <<<<<<<<<<<<<\e[0m" | tee -a ${log}
 
-cd /app
-mvn clean package
-mv target/${component}-1.0.jar ${component}.jar
+cd /app &>> ${log}
+mvn clean package &>> ${log}
+mv target/${component}-1.0.jar ${component}.jar &>> ${log}
 
 echo -e "\e[31m>>>>>>  Installing MYSQL <<<<<<<<<<<<<\e[0m" | tee -a ${log}
-yum install mysql -y
+yum install mysql -y &>> ${log}
 
 echo -e "\e[31m>>>>>>  Load Schema   <<<<<<<<<<<<<\e[0m" | tee -a ${log}
 
-mysql -h mysql-dev.pdevopst74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql
+mysql -h mysql-dev.pdevopst74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>> ${log}
 
 func_systemd
 }
