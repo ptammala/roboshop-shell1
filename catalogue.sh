cb82962b0f9d42ecf2e28b2223430b79f972ef37
github-repos <sheshankprakash260@gmail.com>
2023-08-01 21:43:11 +0530

    Updating log file

--- catalogue.sh
+++ catalogue.sh
@@ -1,50 +1,52 @@
 
-echo -e "\e[31m>>>>>>  Create  a  Catalogue Service File <<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-cp catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/roboshop.log
+log=/tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>   Create a MongoDb Reo File <<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
-cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>  Create  a  Catalogue Service File <<<<<<<<<<<<<\e[0m" | tee -a ${log}
+cp catalogue.service /etc/systemd/system/catalogue.service &>> ${log}
 
-echo -e "\e[31m>>>>>>   Installing Nodejs Repo File  <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>   Create a MongoDb Reo File <<<<<<<<<<<<<\e[0m" | tee -a ${log}
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> ${log}
 
-echo -e "\e[31m>>>>>>  Installing Nodejs <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-yum install nodejs -y  &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>   Installing Nodejs Repo File  <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> ${log}
 
-echo -e "\e[31m>>>>>>  Create Application User  <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-useradd roboshop &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>  Installing Nodejs <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+yum install nodejs -y  &>> ${log}
 
-echo -e "\e[31m>>>>>>  Cleaning Application  Content <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-rm -rf /app &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>  Create Application User  <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+useradd roboshop &>> ${log}
 
-echo -e "\e[31m>>>>>>  Create Application Directory <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-mkdir /app &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>  Cleaning Application  Content <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+rm -rf /app &>> ${log}
 
-echo -e "\e[31m>>>>>>   Downloading Application Content <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>  Create Application Directory <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+mkdir /app &>> ${log}
 
-
-
-echo -e "\e[31m>>>>>>  Extracting Application Content <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-cd /app  &>> /tmp/roboshop.log
-unzip /tmp/catalogue.zip  &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>   Downloading Application Content <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> ${log}
 
 
-echo -e "\e[31m>>>>>> Installing Application Dependencies <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-cd /app  &>> /tmp/roboshop.log
-npm install  &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>  Reloading a Deamon   <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-systemctl daemon-reload   &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>  Extracting Application Content <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+cd /app  &>> ${log}
+unzip /tmp/catalogue.zip  &>> ${log}
 
-echo -e "\e[31m>>>>>>  Starting a Catalogue Service <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-systemctl enable catalogue  &>> /tmp/roboshop.log
-systemctl start catalogue  &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>  Installing Mongodb Client Shell <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-yum install mongodb-org-shell -y  &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>> Installing Application Dependencies <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+cd /app  &>> ${log}
+npm install  &>> ${log}
 
-echo -e "\e[31m>>>>>>   Load  a Cataloguie Schema  <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
-mongo --host mongodb-dev.pdevopst74.online </app/schema/catalogue.js  &>> /tmp/roboshop.log
+echo -e "\e[31m>>>>>>  Reloading a Deamon   <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+systemctl daemon-reload   &>> ${log}
+
+echo -e "\e[31m>>>>>>  Starting a Catalogue Service <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+systemctl enable catalogue  &>> ${log}
+systemctl start catalogue  &>> ${log}
+
+echo -e "\e[31m>>>>>>  Installing Mongodb Client Shell <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+yum install mongodb-org-shell -y  &>> ${log}
+
+echo -e "\e[31m>>>>>>   Load  a Cataloguie Schema  <<<<<<<<<<<<<\e[0m"  | tee -a ${log}
+mongo --host mongodb-dev.pdevopst74.online </app/schema/catalogue.js  &>> ${log}
 
 
