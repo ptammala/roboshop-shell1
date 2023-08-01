65f9bc0ef81fb791c3eb28ad530321af244d7cdf
github-repos <sheshankprakash260@gmail.com>
2023-08-01 21:00:45 +0530

    Updating echo

--- catalogue.sh
+++ catalogue.sh
@@ -1,50 +1,50 @@
 
-echo -e "\e[31m>>>>>>  Create  a  Catalogue Service File <<<<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[31m>>>>>>  Create  a  Catalogue Service File <<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cp catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>   Create a MongoDb Reo File <<<<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[31m>>>>>>   Create a MongoDb Reo File <<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>   Installing Nodejs Repo File  <<<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[31m>>>>>>   Installing Nodejs Repo File  <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>  Installing Nodejs <<<<<<<<<<<<<\e[0m"
-yum install nodejs -y
+echo -e "\e[31m>>>>>>  Installing Nodejs <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+yum install nodejs -y  &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>  Create Application User  <<<<<<<<<<<<<\e[0m"
-useradd roboshop
+echo -e "\e[31m>>>>>>  Create Application User  <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+useradd roboshop &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>  Cleaning Application  Content <<<<<<<<<<<<<\e[0m"
-rm -rf /app
+echo -e "\e[31m>>>>>>  Cleaning Application  Content <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+rm -rf /app &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>  Create Application Directory <<<<<<<<<<<<<\e[0m"
-mkdir /app
+echo -e "\e[31m>>>>>>  Create Application Directory <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+mkdir /app &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>   Downloading Application Content <<<<<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-
-
-
-echo -e "\e[31m>>>>>>  Extracting Application Content <<<<<<<<<<<<<\e[0m"
-cd /app
-unzip /tmp/catalogue.zip
+echo -e "\e[31m>>>>>>   Downloading Application Content <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/roboshop.log
 
 
-echo -e "\e[31m>>>>>> Installing Application Dependencies <<<<<<<<<<<<<\e[0m"
-cd /app
-npm install
 
-echo -e "\e[31m>>>>>>  Reloading a Deamon   <<<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
+echo -e "\e[31m>>>>>>  Extracting Application Content <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+cd /app  &>> /tmp/roboshop.log
+unzip /tmp/catalogue.zip  &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>  Starting a Catalogue Service <<<<<<<<<<<<<\e[0m"
-systemctl enable catalogue
-systemctl start catalogue
 
-echo -e "\e[31m>>>>>>  Installing Mongodb Client Shell <<<<<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
+echo -e "\e[31m>>>>>> Installing Application Dependencies <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+cd /app  &>> /tmp/roboshop.log
+npm install  &>> /tmp/roboshop.log
 
-echo -e "\e[31m>>>>>>   Load  a Cataloguie Schema  <<<<<<<<<<<<<\e[0m"
-mongo --host mongodb-dev.pdevopst74.online </app/schema/catalogue.js
+echo -e "\e[31m>>>>>>  Reloading a Deamon   <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+systemctl daemon-reload   &>> /tmp/roboshop.log
+
+echo -e "\e[31m>>>>>>  Starting a Catalogue Service <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+systemctl enable catalogue  &>> /tmp/roboshop.log
+systemctl start catalogue  &>> /tmp/roboshop.log
+
+echo -e "\e[31m>>>>>>  Installing Mongodb Client Shell <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+yum install mongodb-org-shell -y  &>> /tmp/roboshop.log
+
+echo -e "\e[31m>>>>>>   Load  a Cataloguie Schema  <<<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+mongo --host mongodb-dev.pdevopst74.online </app/schema/catalogue.js  &>> /tmp/roboshop.log
 
 
