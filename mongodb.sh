3c7b0457b9a8045193471f49daae7960e4c43459
github-repos <sheshankprakash260@gmail.com>
2023-08-26 00:49:42 +0530

    Mongodb yml

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl restart mongod
